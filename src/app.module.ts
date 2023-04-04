3287344d9f973dab3bd748e612976a6f6ecd6a33
Théo DURR <git@theodurr.fr>
2023-04-04 15:11:44 +0200

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HelloResolver } from './hello/hello.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
+  providers: [HelloResolver],
+})
+export class AppModule {}
---
+++ src/hello/hello.resolver.ts
@@ -0,0 +1,9 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class HelloResolver {
+  @Query((returns) => String)
+  async hello() {
+    return 'Hello World !';
+  }
+}
