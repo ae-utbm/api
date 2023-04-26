1b646a226bab54b5bd1ace016cd4750f4740c09d
Julien Constant <julienconstant190@gmail.com>
2023-04-26 22:54:12 +0200

    Added linter & vscode config to match sith4 repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,13 @@ import { join } from 'path';
 import { HelloResolver } from './hello/hello.resolver';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-    }),
-  ],
-  providers: [HelloResolver],
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+			sortSchema: true,
+		}),
+	],
+	providers: [HelloResolver],
 })
 export class AppModule {}
--- src/hello/hello.resolver.ts
+++ src/hello/hello.resolver.ts
@@ -2,8 +2,8 @@ import { Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloResolver {
-  @Query((returns) => String)
-  async hello() {
-    return 'Hello World !';
-  }
+	@Query((returns) => String)
+	public async hello() {
+		return 'Hello World !';
+	}
 }
