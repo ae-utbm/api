37882b97222969eb28abf1c1479ea91e65fd4913
Julien Constant <julienconstant190@gmail.com>
2023-08-24 12:06:05 +0200

    Specify user in the id field for clarity

--- src/modules/logs/logs.controller.ts
+++ src/modules/logs/logs.controller.ts
@@ -30,22 +30,22 @@ import { LogsService } from './logs.service';
 export class LogsController {
 	constructor(private readonly logsService: LogsService, private readonly i18n: I18nService<I18nTranslations>) {}
 
-	@Get('user/:id')
+	@Get('user/:user_id')
 	@UseGuards(SelfOrPermissionGuard)
-	@GuardSelfOrPermissions('id', ['CAN_READ_LOGS_OF_USER'])
+	@GuardSelfOrPermissions('user_id', ['CAN_READ_LOGS_OF_USER'])
 	@ApiNotFoundResponse({ description: 'User not found' })
 	@ApiBadRequestResponse({ description: 'Invalid user ID' })
 	@ApiOkResponse({ description: 'User logs retrieved', type: [Log] })
 	@ApiParam({ name: 'id', description: 'The user ID' })
 	@ApiOperation({ summary: 'Get all logs of a user' })
-	getUserLogs(@Param('id') id: number) {
+	getUserLogs(@Param('user_id') id: number) {
 		if (typeof id !== 'number' && parseInt(id, 10) != id)
 			throw new BadRequestException(Errors.Generic.FieldInvalid({ i18n: this.i18n, type: Number, field: 'id' }));
 
 		return this.logsService.getUserLogs(id);
 	}
 
-	@Delete('user/:id')
+	@Delete('user/:user_id')
 	@UseGuards(PermissionGuard)
 	@GuardPermissions('CAN_DELETE_LOGS_OF_USER')
 	@ApiNotFoundResponse({ description: 'User not found' })
@@ -53,7 +53,7 @@ export class LogsController {
 	@ApiOkResponse({ description: 'User logs deleted', type: MessageResponseDTO })
 	@ApiParam({ name: 'id', description: 'The user ID' })
 	@ApiOperation({ summary: 'Delete all logs of a user' })
-	deleteUserLogs(@Param('id') id: number) {
+	deleteUserLogs(@Param('user_id') id: number) {
 		if (typeof id !== 'number' && parseInt(id, 10) != id)
 			throw new BadRequestException(Errors.Generic.FieldInvalid({ i18n: this.i18n, type: Number, field: 'id' }));
 
