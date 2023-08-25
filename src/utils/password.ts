e48b6628a5b0c7d4f0a18dfc84d396113c6e3c50
Julien Constant <julienconstant190@gmail.com>
2023-08-25 19:52:16 +0200

    Fix insecure randomness

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,9 +1,27 @@
+import * as crypto from 'crypto';
+
 const SPECIAL_CHARS = '!@#$%^&*()';
 const LOWERCASE_CHARS = 'abcdefghijklmnopqrstuvwxyz';
 const UPPERCASE_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const NUMBERS = '0123456789';
 
 const MINIMUM_PASSWORD_LENGTH = 8;
+
+/**
+ * Generates a random integer between min and max (inclusive).
+ * @param {number} max @default 100
+ * @param {number} min @default 0
+ * @returns {number} the generated random integer
+ */
+export function secureRandomInt(max: number = 100, min: number = 0): number {
+	const range = max - min + 1;
+	const bitsNeeded = Math.ceil(Math.log2(range));
+	const bytesNeeded = Math.ceil(bitsNeeded / 8);
+	const randomBytes = crypto.randomBytes(bytesNeeded);
+	const randomValue = randomBytes.readUIntLE(0, bytesNeeded);
+
+	return min + (randomValue % range);
+}
 
 /**
  * Generates a random password of the given length.
@@ -14,10 +32,10 @@ export function generateRandomPassword(length: number = MINIMUM_PASSWORD_LENGTH)
 	if (length < MINIMUM_PASSWORD_LENGTH) length = MINIMUM_PASSWORD_LENGTH;
 
 	const password = [
-		SPECIAL_CHARS[Math.floor(Math.random() * SPECIAL_CHARS.length)],
-		LOWERCASE_CHARS[Math.floor(Math.random() * LOWERCASE_CHARS.length)],
-		UPPERCASE_CHARS[Math.floor(Math.random() * UPPERCASE_CHARS.length)],
-		NUMBERS[Math.floor(Math.random() * NUMBERS.length)],
+		SPECIAL_CHARS[secureRandomInt(SPECIAL_CHARS.length - 1)],
+		LOWERCASE_CHARS[secureRandomInt(LOWERCASE_CHARS.length - 1)],
+		UPPERCASE_CHARS[secureRandomInt(UPPERCASE_CHARS.length - 1)],
+		NUMBERS[secureRandomInt(NUMBERS.length - 1)],
 	];
 
 	const remainingLength = length - 4;
