719d313d2c7ffe94c66e56163aeb3aaa2f430a49
Julien Constant <julienconstant190@gmail.com>
2023-08-25 19:50:50 +0200

    Fix inefficient regular expression

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -8,6 +8,8 @@ import env from '@env';
 
 /**
  * Check if an email is allowed to be used (to register for example) and if it is valid
+ * @see https://emailregex.com/
+ *
  * @param {email} email the email to check
  * @returns {boolean} true if the email is allowed, false otherwise
  */
@@ -18,7 +20,9 @@ export function checkEmail(email: email): boolean {
 	if (whitelisted.includes(email)) return true;
 	else if (blacklist.some((host) => email.endsWith(host))) return false;
 
-	const regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+	const regex = new RegExp(
+		/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+	);
 	return regex.test(email);
 }
 
