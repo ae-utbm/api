a9ee28f7a6d5f00928028e54a8cb48512e586d6e
Julien Constant <julienconstant190@gmail.com>
2023-09-20 00:20:27 +0200

    Add status code to MessageResponseDTO

--- src/modules/_mixin/dto/message-response.dto.ts
+++ src/modules/_mixin/dto/message-response.dto.ts
@@ -1,10 +1,14 @@
 import type { MessageResponseDto } from '#types/api';
 
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class MessageResponseDTO implements MessageResponseDto {
 	@ApiProperty()
 	@IsString()
 	message: string;
+
+	@ApiProperty({ example: 200 })
+	@IsNumber()
+	statusCode: number;
 }
